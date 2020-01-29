4f374713921a7fc53263d4f245e604330eb5b1ff
Josh Couper <joshcouper@gmail.com>
2020-01-29 21:07:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
   	                           :names => 
   	                                {:reading_railroad => {}, 
   	                                 :pennsylvania_railroad => {}, 
-  	                                 :name3 => "name 3", 
+  	                                 :b_and_o_railroad => {}, 
   	                                 :name4 => "name 4"}, 
   	                   }
   	 } 
